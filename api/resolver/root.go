442a444a373ff8281d236835654aec2362c123f0
David Herrmann <davherrmann@googlemail.com>
2019-01-06 17:51:00 +0100

    add mutations

--- api/resolver/root.go
+++ api/resolver/root.go
@@ -32,3 +32,20 @@ func (r *Root) Orders(ctx context.Context) []Order {
 	}
 	return orders
 }
+
+// OrderFood mutation
+func (r *Root) OrderFood(ctx context.Context, args *struct {
+	Date string
+	Food string
+}) (bool, error) {
+	log.Println("order food " + args.Date + " " + args.Food)
+	return true, nil
+}
+
+// CancelFoodOrder mutation
+func (r *Root) CancelFoodOrder(ctx context.Context, args *struct {
+	Date string
+}) (bool, error) {
+	log.Println("cancel food order " + args.Date)
+	return true, nil
+}
